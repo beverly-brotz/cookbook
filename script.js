5f6b82e8c0a479f61c87a61824d2bdcb4ce0f709
Beverly Brotz <160075262+beverly-brotz@users.noreply.github.com>
2024-04-26 13:42:01 -0500

    Added button functions, fixed some positioning

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-let recipeWindow = document.getElementById("recipe-window")
+let recipeContent = document.getElementById('recipe-html')
+let pageTitle = document.getElementById('page-title')
+
+
+/* functions for buttons - swaps html on index page to show different recipes*/
+
+function chiliGet() {
+  fetch('/recipes/chili.html').then(function(response) {
+    return response.text()
+  }).then(function(html) {
+    var parser = new DOMParser();
+    var doc = parser.parseFromString(html, 'text/html');
+    var recipe = doc.querySelector('#chili-html').innerHTML;
+    recipeContent.innerHTML = recipe;
+  }).catch(function(err){
+    console.log('vailed to fetch page: ', err);
+  });
+  pageTitle.innerText = "Chili Recipe"
+}
+
+function meatloafGet() {
+  fetch('/recipes/meatloaf.html').then(function(response) {
+    return response.text()
+  }).then(function(html) {
+    var parser = new DOMParser();
+    var doc = parser.parseFromString(html, 'text/html');
+    var recipe = doc.querySelector('#meatloaf-html').innerHTML;
+    recipeContent.innerHTML = recipe;
+  }).catch(function(err){
+    console.log('vailed to fetch page: ', err);
+  });
+  pageTitle.innerText = "Meatloaf Recipe"
+}
+function stuffedpeppersGet() {
+  fetch('/recipes/stuffedpeppers.html').then(function(response) {
+    return response.text()
+  }).then(function(html) {
+    var parser = new DOMParser();
+    var doc = parser.parseFromString(html, 'text/html');
+    var recipe = doc.querySelector('#stuffedpeppers-html').innerHTML;
+    recipeContent.innerHTML = recipe;
+  }).catch(function(err){
+    console.log('vailed to fetch page: ', err);
+  });
+  pageTitle.innerText = "Stuffed Peppers Recipe"
+}
